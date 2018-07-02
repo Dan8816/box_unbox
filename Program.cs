0a786dbaeec4479cc6a0bf323d1cef39095188f9
Dan8816 <dan881631537518@gmail.com>
2018-07-02 18:00:24 -0500

    1st commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace box_unbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> BoxedData = new List<object>();
+            BoxedData.Add(7);
+            BoxedData.Add(28);
+            BoxedData.Add(-1);
+            BoxedData.Add(true);
+            BoxedData.Add("Chair");
+            var sum = 0;
+            foreach (object box in BoxedData)
+            {
+                Console.WriteLine(box);
+                if (box is int)
+                {
+                    sum += (int)box;
+                };
+            };
+            Console.WriteLine(sum); 
+        }
+    }
+}
